5a2f5a77f9d1b1d2650c09d636fe9c9604f28c9f
NevidAmrullah <nevidamrullah24@gmail.com>
2024-08-14 11:55:11 +0700

    perbaiki bug hasil

--- js/script.js
+++ js/script.js
@@ -3,11 +3,12 @@ function hitungBmi() {
     var angka1 = parseFloat(document.getElementById("berat-badan").value);
     var angka2 = parseFloat(document.getElementById("tinggi-badan").value);
     var konversiKeMeter = angka2 / 100;
+//Konversi tinggi badan ke meter 
     var hasil = angka1 / (konversiKeMeter ** 2);
-
+//meng-outputkan hasil perhitungan ke html
     var printOutput = document.getElementById("hasil-output");
     printOutput.innerHTML = hasil.toFixed(2);
-
+//program if else untuk menentukan kategori badan ideal dan tidak ideal
     if (hasil <= 18.5) {
         var status = document.getElementById("keterangan");
         status.innerHTML = "Kekurangan Berat badan";
@@ -20,7 +21,7 @@ function hitungBmi() {
         var status = document.getElementById("keterangan");
         status.innerHTML = "Kelebihan berat badan";
     }
-    else  {
+    else if (hasil > 30.0) {
         var status = document.getElementById("keterangan");
         status.innerHTML = "Kegemukan (obesitas)"
     }
@@ -30,6 +31,5 @@ function hitungBmi() {
 
 function reset() {
     var reset = document.getElementsByTagNameNS("input");
-    reset.innerHTML
-
+    reset.innerHTML = ""
 }
